e7d6b222b81d01cc080e72c11818cad1ce28010b
David Puerto Guerrero <dsp5502@gmail.com>
2022-04-11 21:44:21 -0500

    terminado descripcion

--- src/components/productSelect/Descripcion.js
+++ src/components/productSelect/Descripcion.js
@@ -9,36 +9,38 @@ const Descripcion = () => {
       </h1>
       <span className='text-emerald-400 py-3 '>Marca: Canon</span>
       <hr />
-      <div className=''>
-        <p>Precio:</p>
-        <p>$100,669.00</p>
+      <div className='flex justify-between mb-3 text-sm'>
+        <p className=''>Precio:</p>
+        <p className='text-red-600 '>$100,669.00</p>
         <p>
           {' '}
-          <strong>Envío GRATIS</strong>{' '}
+          <strong>Envío GRATIS.</strong>{' '}
         </p>
-        <span>Detalles</span>
+        <span className='text-emerald-400'>Detalles</span>
       </div>
-      <p>
-        Hasta 18 meses sin intereses de $5,592.76.{' '}
-        <span>Ver mensualidades Solicita tu tarjeta Amazon Recargable</span> y
-        obtén $100 de descuento en tu primera compra mayor a $500
+      <p className='text-sm'>
+        Hasta <strong>18 meses sin intereses</strong> de $5,592.76.{' '}
+        <span className='text-emerald-400'>
+          Ver mensualidades Solicita tu tarjeta Amazon Recargable
+        </span>{' '}
+        y obtén $100 de descuento en tu primera compra mayor a $500
       </p>
-      <span>
+      <span className='my-2'>
         Precio: <strong>Negro</strong>{' '}
       </span>
-      <hr />
-      <span>
+      <span className='my-2'>
         Precio: <strong>24-105mm USM Kit</strong>{' '}
       </span>
       <hr />
-      <h5>Acerca de este artículo</h5>
-      <p>
+      <h5 className='my-2 font-bold text-lg'>Acerca de este artículo</h5>
+      <p className='my-2 text-sm leading-8'>
         - Alta calidad de imagen con un nuevo sensor CMOS de marco completo de
-        20 megapíxeles. - Procesador de imagen DIGIC X con una gama ISO de
-        100-102400; ampliable a 204800. - Disparo continuo de alta velocidad de
-        hasta 12 fps con obturador mecánico y obturador electrónico (silencioso)
-        de hasta 20 fps. - CMOS AF de doble pixel, cubre aproximadamente Área
-        100% con 1.053 AF. - Seguimiento de asuntos de personas y animales*
+        20 megapíxeles. <br /> - Procesador de imagen DIGIC X con una gama ISO
+        de 100-102400; ampliable a 204800.
+        <br /> - Disparo continuo de alta velocidad de hasta 12 fps con
+        obturador mecánico y obturador electrónico (silencioso) de hasta 20 fps.
+        <br /> - CMOS AF de doble pixel, cubre aproximadamente Área 100% con
+        1.053 AF. <br /> - Seguimiento de asuntos de personas y animales*
         utilizando tecnología de aprendizaje profundo.
       </p>
     </div>
