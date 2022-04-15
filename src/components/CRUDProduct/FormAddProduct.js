2c4fb22fc796dab559adb0c58faa3f0c45cf93a1
David Puerto Guerrero <dsp5502@gmail.com>
2022-04-14 22:20:19 -0500

    correcion mobil forumlario producto

--- src/components/CRUDProduct/FormAddProduct.js
+++ src/components/CRUDProduct/FormAddProduct.js
@@ -5,7 +5,7 @@ import ProductFormAdd from './ProductFormAdd';
 const FormAddProduct = () => {
   return (
     <div className=' w-full  flex  flex-col justify-center items-center h-screen mx-auto mt-5'>
-      <div className=' w-2/3 md:w-1/4   h-5/6 flex flex-col items-center justify-center '>
+      <div className=' w-full px-2 md:w-3/4   h-5/6 flex flex-col items-center justify-center '>
         <div className=' mr-2 md:mr-0 mt-2 md:mt-0 flex flex-col  justify-center  items-center my-5'>
           <h1 className='text-sm md:text-2xl fontLogo '>Amazonas</h1>
           <img
@@ -17,18 +17,6 @@ const FormAddProduct = () => {
         <div className='borderEnvio w-full  flex flex-col px-5 py-2'>
           <h2 className=' text-xl mb-1'>Ingresar</h2>
           <ProductFormAdd />
-
-          <p className='text-sm my-3'>
-            Al continuar, aceptas las{' '}
-            <span className='text-blue-500'>Condiciones de uso</span> y el{' '}
-            <span className='text-blue-500'>Aviso de privacidad</span> de
-            Amazonas{' '}
-          </p>
-        </div>
-        <div className='text-sm flex flex-col items-center w-full my-3'>
-          <p className=''>
-            <span>¿nuevo en amazon?</span>
-          </p>
         </div>
       </div>
     </div>
