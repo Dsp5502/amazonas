254d3581ccf6a6939085394e5f9b7e7f3316c2a7
David Puerto Guerrero <dsp5502@gmail.com>
2022-04-15 12:36:48 -0500

    arreglo de fecha de entrega

--- src/components/productSelect/Envio.js
+++ src/components/productSelect/Envio.js
@@ -1,8 +1,78 @@
 import { faCartShopping, faPlay } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import dateJs from 'date.js';
 import React from 'react';
 
 const Envio = ({ precio }) => {
+  const diaPasado = dateJs('5 days after');
+  const diaPasadoFin = dateJs('7 days after');
+  const diaAprox = diaPasado.getMonth() + 1;
+  let dia;
+  switch (diaAprox) {
+    case 1:
+      dia = `Enero,  
+        ${diaPasado.getDate()} - ${diaPasadoFin.getDate()}`;
+      break;
+
+    case 2:
+      dia = `Feb,
+        ${diaPasado.getDate()} - ${diaPasadoFin.getDate()}`;
+      break;
+
+    case 3:
+      dia = `Marzo,
+        ${diaPasado.getDate()} - ${diaPasadoFin.getDate()}`;
+      break;
+
+    case 4:
+      dia = `Abril,
+        ${diaPasado.getDate()} - ${diaPasadoFin.getDate()}`;
+      break;
+
+    case 5:
+      dia = `Mayo,
+        ${diaPasado.getDate()} - ${diaPasadoFin.getDate()}`;
+      break;
+
+    case 6:
+      dia = `Junio,
+        ${diaPasado.getDate()} - ${diaPasadoFin.getDate()}`;
+      break;
+
+    case 7:
+      dia = `Julio,
+        ${diaPasado.getDate()} - ${diaPasadoFin.getDate()}`;
+      break;
+
+    case 8:
+      dia = `Agost,
+        ${diaPasado.getDate()} - ${diaPasadoFin.getDate()}`;
+      break;
+
+    case 9:
+      dia = `Sept,
+        ${diaPasado.getDate()} - ${diaPasadoFin.getDate()}`;
+      break;
+
+    case 10:
+      dia = `Oct,
+        ${diaPasado.getDate()} - ${diaPasadoFin.getDate()}`;
+      break;
+
+    case 11:
+      dia = `Nov,
+        ${diaPasado.getDate()} - ${diaPasadoFin.getDate()}`;
+      break;
+
+    case 12:
+      dia = `Dic,
+        ${diaPasado.getDate()} - ${diaPasadoFin.getDate()}`;
+      break;
+
+    default:
+      break;
+  }
+
   return (
     <div className='borderEnvio w-11/12 flex flex-col mx-auto mt-6 py-2 justify-center px-2 '>
       <p className='text-red-600 text-2xl my-2'>${precio}</p>
@@ -11,18 +81,14 @@ const Envio = ({ precio }) => {
         <span className='font-light text-emerald-400 '>Detalles</span>{' '}
       </p>
       <p className='my-2'>
-        Llega:{' '}
+        Llega :{' '}
         <span>
           {' '}
-          <strong>dic 25- 28</strong>{' '}
+          <strong>{dia}</strong>{' '}
         </span>{' '}
       </p>
-      <p className='text-red-600 text-sm my-3'>
-        Puede que lo recibas después de Navidados
-      </p>
-      <div>
-        <div></div>
 
+      <div>
         <button className='rounded-sm w-11/12 m-2 py-2 bg-amber-500  flex justify-evenly'>
           <FontAwesomeIcon
             className='text-white bg-gray-500 '
